27a7566712b2ff76ddd41747f4d363bfb3bdef0d
Shreshth Dewangan <96073792+KewlShrezhth@users.noreply.github.com>
2025-09-28 12:49:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+GA_MEASUREMENT_ID = os.environ.get("GA_MEASUREMENT_ID")  # will be set on Render
+
+@app.context_processor
+def inject_ga():
+    return dict(GA_MEASUREMENT_ID=GA_MEASUREMENT_ID)
 
 @app.route('/')
 def home():
@@ -8,7 +15,5 @@ def home():
     moods = ["upset", "angry", "nervous", "missing me", "insecure", "broken up"]
     return render_template('index.html', moods=moods)
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Render gives you a port
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
