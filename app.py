a252735d0e0032605e373b0b53bacceb518adc75
Shreshth Dewangan <96073792+KewlShrezhth@users.noreply.github.com>
2025-09-28 13:07:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ def home():
     return render_template('index.html', moods=moods)
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render gives the port
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
